933b620bd402dad9a4c417c47cf451284a8028ff
tconuorah <163480467+tconuorah@users.noreply.github.com>
2025-09-20 12:44:40 -0500

    Delete terraform directory

--- terraform/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "alb_ingress_hostname" {
-  description = "ALB Ingress hostname for tc2-web"
-  value       = try(kubernetes_ingress_v1.web.status[0].load_balancer[0].ingress[0].hostname, "")
-}
-
-output "namespace" {
-  value = kubernetes_namespace.app.metadata[0].name
-}
